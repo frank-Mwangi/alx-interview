e86188379e663f80a5f42f594bd601a6ee683a29
frank-Mwangi <kungofrank@gmail.com>
2023-08-02 21:45:25 +0300

    Lockboxes interview question

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,13 +5,16 @@ Function to check if a series of lockboxes can all be accessed
 
 
 def canUnlockAll(boxes):
+    if not any(isinstance(box, list) for box in boxes):
+        return False
     n = len(boxes)
-    visited = set()
-    stack = [0]
-    while stack:
-        box = stack.pop()
-        visited.add(box)
-        for key in boxes[box]:
-            if key not in visited and 0 <= key < n:
-                stack.append(key)
-    return len(visited) == n
+    if n == 1:
+        return True
+    open_boxes = [0]
+    keys_available = boxes[0].copy()
+    while keys_available:
+        key = keys_available.pop()
+        if key not in open_boxes and 0 <= key < n:
+            open_boxes.append(key)
+            keys_available.extend(boxes[key])
+    return len(open_boxes) == n

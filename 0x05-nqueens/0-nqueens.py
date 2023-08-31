7147221412ec20c4d98be7f65df91a96dc9efe3a
frank-Mwangi <kungofrank@gmail.com>
2023-08-31 08:28:42 +0300

    N queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -5,61 +5,77 @@ N Queens
 usage: nqueens N
 """
 
-from socket import TIPC_SUBSCR_TIMEOUT
 import sys
 
 
-def is_safe(board, row, col, N):
-    """Check if queen can be placed at given position"""
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    exit(1)
+N = sys.argv[1]
+try:
+    N = int(N)
+
+except:
+    print('N must be a number')
+    exit(1)
+
+k = 1
+
+
+def printSol(board):
+    """Utility function t print solution"""
+    queens = []
+    global k
+    k = k + 1
+    for i in range(N):
+        for j in range(N):
+            if board[i][j] == 1:
+                queens.append([i, j])
+    print(queens)
+
+
+def isSafe(board, row, col):
     for i in range(col):
-        if board[row][i] == 1:
+        if board[row][i]:
             return False
-
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
+    i = row
+    j = col
+    while i >= 0 and j >= 0:
+        if board[i][j]:
             return False
-
-    for i, j in zip(range(row, N), range(col, -1, -1)):
-        if board[i][j] == 1:
+        i -= 1
+        j -= 1
+    i = row
+    j = col
+    while j >= 0 and i < N:
+        if board[i][j]:
             return False
-
+        i = i + 1
+        j = j - 1
     return True
 
 
-def solve_nqueens_util(board, col, N):
-    if col >= N:
-        """Print solution if all queens are placed"""
-        for i in range(N):
-            for j in range(N):
-                if board[i][j] == 1:
-                    print(f'[{i}, {j}]', end=" ")
-        print()
-        return
-
+def solveNQUtil(board, col):
+    """solve n queens"""
+    if col == N:
+        printSol(board)
+        return True
+    res = False
     for i in range(N):
-        if is_safe(board, i, col, N):
+        if isSafe(board, i, col):
             board[i][col] = 1
-            solve_nqueens_util(board, col + 1, N)
+            res = solveNQUtil(board, col + 1) or res
             board[i][col] = 0
+    return res
 
 
-def solve_nqueens(N):
-    if not N.isdigit():
-        print("N must be a number")
-        sys.exit(1)
-
-    N = int(N)
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
-    board = [[0 for _ in range(N)] for _ in range(N)]
-    solve_nqueens_util(board, 0, N)
+def solveNQ():
+    """Methos to solve NQ"""
+    board = [[0 for j in range(N)] for i in range(N)]
+    if solveNQUtil(board, 0) is False:
+        pass
+        return
+    return
 
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-
-    solve_nqueens(sys.argv[1])
+solveNQ()

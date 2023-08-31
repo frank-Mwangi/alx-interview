a3693c4740db37a7172a009f3d7f943e49ec8034
frank-Mwangi <kungofrank@gmail.com>
2023-08-31 07:52:00 +0300

    N queens

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""
+N Queens
+
+usage: nqueens N
+"""
+
+from socket import TIPC_SUBSCR_TIMEOUT
+import sys
+
+
+def is_safe(board, row, col, N):
+    """Check if queen can be placed at given position"""
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    for i, j in zip(range(row, N), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+
+def solve_nqueens_util(board, col, N):
+    if col >= N:
+        """Print solution if all queens are placed"""
+        for i in range(N):
+            for j in range(N):
+                if board[i][j] == 1:
+                    print(f'[{i}, {j}]', end=" ")
+        print()
+        return
+
+    for i in range(N):
+        if is_safe(board, i, col, N):
+            board[i][col] = 1
+            solve_nqueens_util(board, col + 1, N)
+            board[i][col] = 0
+
+
+def solve_nqueens(N):
+    if not N.isdigit():
+        print("N must be a number")
+        sys.exit(1)
+
+    N = int(N)
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [[0 for _ in range(N)] for _ in range(N)]
+    solve_nqueens_util(board, 0, N)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    solve_nqueens(sys.argv[1])
---
+++ 0x05-nqueens/README.md
@@ -0,0 +1,1 @@
+N queens - Algorithm to place n queens on an n * n chess board such that no queen is attacking another

017065a14d37742b631f93cf17b8511a2cf64c13
frank-Mwangi <kungofrank@gmail.com>
2023-09-07 19:09:36 +0300

    Star wars character names from API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,37 +1,45 @@
 #!/usr/bin/node
 
-const request = require("request");
-const process = require("process");
+const request = require('request');
+const process = require('process');
 
-function getMovieCharacters(movieId) {
-  const baseUrl = "https://swapi.dev/api";
+async function getMovieCharacters (movieId) {
+  const baseUrl = 'https://swapi.dev/api';
   const movieUrl = `${baseUrl}/films/${movieId}/`;
 
-  request(movieUrl, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-      const movieData = JSON.parse(body);
-      const characterUrls = movieData.characters;
-
-      characterUrls.forEach((characterUrl) => {
-        request(characterUrl, (error, response, body) => {
+  try {
+    const movieData = await new Promise((resolve, reject) => {
+      request(movieUrl, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+          resolve(JSON.parse(body));
+        } else {
+          console.error(`Failed to fetch movie data for movie ID ${movieId}`);
+        }
+      });
+    });
+    const characterPromises = movieData.characters.map((characterURL) => {
+      return new Promise((resolve, reject) => {
+        request(characterURL, (error, response, body) => {
           if (!error && response.statusCode === 200) {
             const characterData = JSON.parse(body);
-            console.log(characterData.name);
+            resolve(characterData.name);
           } else {
-            console.error(`Failed to fetch character for ${characterUrl}`);
+            console.error(`Failed to fetch character for ${characterURL}`);
           }
         });
       });
-    } else {
-      console.error(`Failed to fetch movie data for movie ID ${movieId}`);
+    });
+    const characterNames = await Promise.all(characterPromises);
+    for (const char of characterNames) {
+      console.log(char);
     }
-  });
+  } catch (error) {
+    console.error(error);
+  }
 }
 if (process.argv.length !== 3) {
-  console.log("Usage: node 0-starwars_characters.js <Movie ID");
+  console.log('Usage: node 0-starwars_characters.js <Movie ID>');
   process.exit(1);
 }
-
 const movieId = process.argv[2];
-
 getMovieCharacters(movieId);

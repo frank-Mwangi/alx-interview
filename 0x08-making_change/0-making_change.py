c7749c6ae0e076f38b164783fb02974fec1ab93b
frank-Mwangi <kungofrank@gmail.com>
2023-09-20 12:12:11 +0300

    Greedy algorithm to find least number of coins to make change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,16 +4,32 @@ Find least amount of coins needed to make change
 """
 
 
+# def makeChange(coins, total):
+#     if total <= 0:
+#         return 0
+#     coinsArr = [float('inf')] * (total + 1)
+#     coinsArr[0] = 0
+#     for i in range(1, total + 1):
+#         for coin in coins:
+#             if i >= coin:
+#                 coinsArr[i] = min(coinsArr[i], coinsArr[i - coin] + 1)
+#     if coinsArr[total] == float('inf'):
+#         return -1
+#     else:
+#         return coinsArr[total]
+
+
 def makeChange(coins, total):
     if total <= 0:
         return 0
-    coinsArr = [float('inf')] * (total + 1)
-    coinsArr[0] = 0
-    for i in range(1, total + 1):
-        for coin in coins:
-            if i >= coin:
-                coinsArr[i] = min(coinsArr[i], coinsArr[i - coin] + 1)
-    if coinsArr[total] == float('inf'):
-        return -1
+    coins.sort(reverse=True)
+    coin_count = 0
+
+    for coin in coins:
+        while total >= coin:
+            total -= coin
+            coin_count += 1
+    if total == 0:
+        return coin_count
     else:
-        return coinsArr[total]
+        return -1
